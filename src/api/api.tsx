980646217846ddf043e49bc790c4dda88ec64ff6
Snega <snega14102001@gmail.com>
2026-03-24 11:54:19 +0530

    fix API URL to Render

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const API = axios.create({
-    baseURL: "https://todo-project-fidu.onrender.com/"
+    baseURL: import.meta.env.VITE_API_URL //https://todo-project-fidu.onrender.com/
 });
 export default API;
